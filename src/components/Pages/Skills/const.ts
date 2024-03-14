6299c2a16b9447ebb3fa03cff0c84b2e5cd03be8
Venu G Soganadgi <soganadgivenu@gmail.com>
2024-03-14 23:17:12 +0530

    skills section changes

--- src/components/Pages/Skills/const.ts
+++ src/components/Pages/Skills/const.ts
@@ -3,6 +3,7 @@ export interface ISkillValues {
   name: string;
   level: string;
   section: string;
+  imageUrl: string;
 }
 
 export enum DevelopmentType {
@@ -21,71 +22,83 @@ export const myCurrentSkills: ISkillValues[] = [
     name: "ReactJs",
     level: Levels.INTERMEDIATE,
     section: DevelopmentType.FRONTEND,
+    imageUrl: '/assests/react.png'
   },
   {
     key: "ts",
     name: "TypeScript",
     level: Levels.INTERMEDIATE,
     section: DevelopmentType.FRONTEND,
+    imageUrl: '/assests/typescript.png'
   },
   {
     key: "js",
     name: "JavaScript",
     level: Levels.INTERMEDIATE,
     section: DevelopmentType.FRONTEND,
+    imageUrl: '/assests/javascript.png'
   },
   {
     key: "html",
     name: "HTML",
     level: Levels.INTERMEDIATE,
     section: DevelopmentType.FRONTEND,
+    imageUrl: '/assests/html.png'
   },
   {
     key: "java",
     name: "Java",
     level: Levels.INTERMEDIATE,
     section: DevelopmentType.BACKEND,
+    imageUrl: '/assests/java.png'
   },
   {
     key: "css",
     name: "CSS",
     level: Levels.BASIC,
     section: DevelopmentType.FRONTEND,
+    imageUrl: '/assests/css.png'
   },
   {
     key: "spring",
     name: "SpringBoot",
     level: Levels.BASIC,
     section: DevelopmentType.BACKEND,
+    imageUrl: '/assests/spring.png'
   },
   {
     key: "node",
     name: "NodeJS",
     level: Levels.BASIC,
     section: DevelopmentType.BACKEND,
+    imageUrl: '/assests/node.png'
   },
   {
     key: "express",
     name: "ExpressJS",
     level: Levels.BASIC,
     section: DevelopmentType.BACKEND,
+    imageUrl: '/assests/express.png'
   },
   {
     key: "mongo",
     name: "MongoDB",
     level: Levels.BASIC,
     section: DevelopmentType.BACKEND,
+    imageUrl: '/assests/mongo.png'
   },
   {
     key: "git",
     name: "GIT",
     level: Levels.BASIC,
     section: DevelopmentType.BACKEND,
+    imageUrl: '/assests/github.png'
   },
   {
     key: "material",
     name: "MateriUI",
     level: Levels.BASIC,
     section: DevelopmentType.BACKEND,
+    imageUrl: '/assests/material.png'
   },
 ];
